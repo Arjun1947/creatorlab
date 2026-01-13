3280b8287d3a4199dd67546f60003dbbcd146e2f
Arjun <arjunsharma194712@gmail.com>
2026-01-13 14:16:06 +0530

    Fix DashboardLayout import path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import DashboardLayout from "./DashboardLayout";
+import DashboardLayout from "./Layouts/DashboardLayout";
 import Dashboard from "./pages/Dashboard";
 import CaptionGenerator from "./pages/CaptionGenerator";
 import BioOptimizer from "./pages/BioOptimizer";
